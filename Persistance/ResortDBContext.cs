ed105985146b4190972bc729fe4784561688e70f
reevanth <kirankumarj6666@gmail.com>
2020-11-24 06:42:20 +0530

    ADD API module

---
+++ Persistance/ResortDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class ResortDBContext : DbContext
+    {
+        public ResortDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // public DbSet<Activity> Activities { get; set; }
+
+        // protected override void OnModelCreation(ModelBuilder builder)
+        // {
+        //     builder.Entity<Activity>()
+        //         .HasData();
+        // }
+    }
+}
